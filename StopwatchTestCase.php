0d8e0f298415386f759513b463157e81ece4db60
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-10-22 17:09:37 +0200

    [TASK] Added abstract test case

---
+++ StopwatchTestCase.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ *
+ * This file is part of the Contractportal system
+ * © 2022 die NetzWerkstatt GmbH & Co. KG <info@die-netzwerkstatt.de>
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ */
+
+namespace MathisBurger\PhpUnitStopwatch;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Testcase that can be used for stopwatch testing
+ */
+abstract class StopwatchTestCase extends TestCase
+{
+
+}
